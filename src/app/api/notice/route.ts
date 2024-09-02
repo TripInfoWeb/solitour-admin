210aae8dd6e46eae74d5346e4a52fcba2c84718e
SK\ssssk <ssssksss@naver.com>
2024-09-02 11:51:12 +0900

    feat: 공지사항 삭제 API

--- src/app/api/notice/route.ts
+++ src/app/api/notice/route.ts
@@ -97,4 +97,34 @@ export async function PUT(request: NextRequest) {
   return response;
 }
 
+/**
+ *  공지사항 삭제
+ */
+export async function DELETE(request: NextRequest) {
+  const access_cookie = request.cookies.get("access_token");
+  if (!access_cookie) {
+    const refresh_cookie = request.cookies.get("refresh_token");
+    if (!refresh_cookie) {
+      // 리프레시 토큰이 없으므로 요청 중단
+      return new NextResponse("Refresh token not found", { status: 403 });
+    }
+    // 리프레시 토큰으로 재발급 받아 재요청 보내기 위한 응답
+    return new NextResponse("Refresh token not found", { status: 401 });
+  }
 
+  const url = new URL(request.url);
+  const id = url.searchParams.get("id");
+
+  const response = await fetch(`${process.env.BACKEND_URL}/api/notice/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: `${access_cookie?.name}=${access_cookie?.value}`,
+    },
+    cache: "no-store",
+  });
+
+  revalidatePath(`/service/notice/${id}`);
+
+  return response;
+}
